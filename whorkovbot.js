cc451de9bd2aeb16012805de970654cb26856871
Me <me@memail.com>
2020-10-24 11:50:03 -0400

    Ping bot

---
+++ whorkovbot.js
@@ -0,0 +1,21 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+const config = require("./config.json")
+
+client.on("ready", () => {
+    console.log("I am ready!");
+});
+
+client.on("message", (message) => {
+    if(!message.content.startsWith(config.prefix) || message.author.bot ) return;
+
+    cmd = message.content.substring(0 + config.prefix.length)
+
+    console.log(cmd);
+    if(cmd.startsWith("ping")) {
+        message.channel.send("pong!")
+    }
+});
+
+client.login(config.token);
