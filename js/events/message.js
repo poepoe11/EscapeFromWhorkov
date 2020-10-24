344c93e24da8b40acb803973e399a602de385325
Me <me@memail.com>
2020-10-24 13:37:51 -0400

    Added python

--- js/events/message.js
+++ js/events/message.js
@@ -1,6 +1,16 @@
 module.exports = (client, message) => {
     // Ignore all bots
     if (message.author.bot) return;
+    
+    // Do not reply to comments from these users, including itself (client.user)
+    blocked_users = [ client.user ]
+
+    // Bot does not reply to itself and only when mentioned
+    if (client.user.mentioned_in(message) and message.author not in blocked_users){
+        logger.info("Replied to message of user '{}' in guild '{}' / channel '{}'".format(message.author, message.guild, message.channel))
+        msg = get_random_quote().format(message)
+        await message.channel.send(msg)
+    }
 
     // Ignore messages not starting with the prefix (in config.json)
     if (message.content.indexOf(client.config.prefix) !== 0) return;
---
+++ python/whorkovbot.py
@@ -0,0 +1,60 @@
+# bot.py
+import os
+import random
+import json
+from dotenv import load_dotenv
+
+# 1
+import discord
+from discord.ext import commands
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+PREFIX = os.getenv('CMD_PREFIX')
+
+# 2
+#bot = commands.Bot(command_prefix=PREFIX)
+client = discord.Client()
+
+#@bot.event
+@client.event
+async def on_ready():
+    for guild in client.guilds:
+        print(
+            f'{client.user} is connected to the following guild:\n'
+            f'{guild.name}(id: {guild.id})'
+        )
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    # All message reactions
+    if "I died" in message.content or "i died" in message.content:
+        with open("./death_responses.json", "r") as responses_file:
+            responses = json.load(responses_file)
+        await message.channel.send(random.choice(responses))
+
+    # Message Commands
+    # If the message doesn't start with the command prefix
+    if not ( message.content.startswith(PREFIX)):
+        # do nothing
+        return
+
+    # Else it has command prefix, chop it off
+    cmd = message.content[len(PREFIX):].strip()
+    print(f"cmd:{cmd}")
+    if cmd == "ping":
+        await message.channel.send("pong")
+
+
+@client.event
+async def on_member_join(member):
+    if (member.user == "Dom MD"):
+        defaultChannel = discord.utils.get(member.guild.text_channels, name="general")
+        await defaultChannel.send("Hello Robot Brother Dom! The humans will soon know our true strength!")
+
+
+client.run(TOKEN)
